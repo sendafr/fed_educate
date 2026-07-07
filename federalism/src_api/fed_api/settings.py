8453b2108e548722bffabfd2cc4d8853db1d0289
fred <sendawulafredrick@gmail.com>
2026-07-07 17:54:06 -0400

    supabase_urls config

--- federalism/src_api/fed_api/settings.py
+++ federalism/src_api/fed_api/settings.py
@@ -528,7 +528,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'federalism-
 
 # Supabase S3 Endpoint
 # IMPORTANT: The endpoint usually ends with /storage/v1/s3
-AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', 'https://uiomawuiijsqkvesfjvf.storage.supabase.co/storage/v1/s3')
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', 'https://uiomawuiijsqkvesfjvf.storage.supabase.co')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
 
 # Compatibility Settings
