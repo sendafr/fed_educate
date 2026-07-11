95f872763535faa6dcfed7ecdde566a7ac2af89b
fred <sendawulafredrick@gmail.com>
2026-07-11 19:28:30 -0400

    start.sh

--- federalism/src_api/start.sh
+++ federalism/src_api/start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Starting Celery Worker..."
-celery -A core worker -l info --concurrency=1 --max-tasks-per-child=1 --time-limit=1200 &
+celery -A fed_api worker -l info --concurrency=1 &
 
 echo "Starting Celery Beat..."
-celery -A core beat -l info &
+celery -A fed_api beat -l info &
 
 echo "Starting Gunicorn..."
-gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 120 --forwarded-allow-ips=*
-
-wait
+gunicorn fed_api.wsgi:application --bind 0.0.0.0:8000 --workers 2
